a06bdb99da931297095d3228268ca348a71cd3fb
Herenot <franciscojavieraquinopiosa@hotmail.com>
2022-05-03 22:12:05 +0200

    README.md Primera prueba commit desde VSCode

---
+++ callsSystem.py
@@ -0,0 +1,4 @@
+#Clase en la que se llevarán a cabo las llamadas al sistema operativo
+class callsSystem:
+    def prueba(self):
+        print("Hello world!")
---
+++ main.py
@@ -0,0 +1,2 @@
+#Clase de prueba en la que se realizarán llamadas a otras clases
+
